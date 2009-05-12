f961a02dec475793877f71aa31bdda99c064f188
KATO Kazuyoshi <kzys@8-p.info>
2009-05-12 10:25:28 +0900

    Multi line error of flymake.

--- emacs.d/init-flymake.el
+++ emacs.d/init-flymake.el
@@ -8,19 +8,23 @@
  (list 'flymake-errline 'flymake-warnline))
 
 
-(defun flymake-display-error-on-minibuffer ()
+(defun flymake-display-err-on-minibuffer ()
   "Displays the error/warning for the current line in the minibuffer"
   (interactive)
   (let* ((line-no             (flymake-current-line-no))
          (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
-         (count               (length line-err-info-list)))
+         (count               (length line-err-info-list))
+         (messages))
     (while (> count 0)
-      (when line-err-info-list
-        (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
-               (full-file  (flymake-ler-full-file (nth (1- count) line-err-info-list)))
-               (text (flymake-ler-text (nth (1- count) line-err-info-list)))
-               (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
-          (message "[%s] %s" line text)))
-      (setq count (1- count)))))
+      (let* ((info (nth (1- count) line-err-info-list))
+             (file (flymake-ler-file info))
+             (text (flymake-ler-text info))
+             (line (flymake-ler-line info))
+             (full-file (flymake-ler-full-file info)))
+        (setq messages (cons (format "%d: %s" line text) messages)))
+      (setq count (1- count)))
+    (if (> (length messages) 0)
+        (message "%s"
+                 (mapconcat 'identity messages "\n")))))
 
-(run-with-idle-timer 1 t 'flymake-display-error-on-minibuffer)
+(run-with-idle-timer 1 t 'flymake-display-err-on-minibuffer)
