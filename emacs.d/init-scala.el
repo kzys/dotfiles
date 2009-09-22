c86eec7d575ccb24f7f0f1c2e1160ed4e1e77987
KATO Kazuyoshi <kzys@8-p.info>
2009-09-22 13:08:49 +0900

    Don't load scala-mode if not installed.

--- emacs.d/init-scala.el
+++ emacs.d/init-scala.el
@@ -1,6 +1,8 @@
-(setq load-path
-      (cons (expand-file-name "~/src/scala-2.7.4.final/misc/scala-tool-support/emacs/") load-path))
-(require 'scala-mode)
-
-(setq auto-mode-alist
-      (cons '("\\.scala$" . scala-mode) auto-mode-alist))
+(let ((path (expand-file-name "~/src/scala-2.7.6.final")))
+  (cond ((file-directory-p path)
+         (setq load-path
+               (cons (concat path "/misc/scala-tool-support/emacs/")
+                     load-path))
+         (require 'scala-mode)
+         (setq auto-mode-alist
+               (cons '("\\.scala$" . scala-mode) auto-mode-alist)))))
