62c169a06400d747e18762f6ba0c292c2b3b2515
KATO Kazuyoshi <kzys@8-p.info>
2009-12-17 00:27:21 +0900

    js2.el -> espresso.el

--- emacs.d/init-web-development.el
+++ emacs.d/init-web-development.el
@@ -6,27 +6,8 @@
 (setq cssm-indent-function #'cssm-c-style-indenter)
 
 ;; JavaScript
-(setq js2-cleanup-whitespace nil
-      js2-mirror-mode nil
-      js2-bounce-indent-flag t
-      js2-auto-indent-flag nil
-      js2-electric-keys nil)
-(when (load "js2" t)
-  ;;(setq-default js2-basic-offset 4)
-
-  (defun indent-and-back-to-indentation ()
-    (interactive)
-    (indent-for-tab-command)
-    (let ((point-of-indentation
-           (save-excursion
-             (back-to-indentation)
-             (point))))
-      (skip-chars-forward "\s " point-of-indentation)))
-
-  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
-
-; (require 'espresso)
-; (add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+(require 'espresso)
+(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
 
 ;; Reload
 (add-hook 'after-save-hook 'reload-browsers)
