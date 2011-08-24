eb343c286306624dec0964ad60902d4c8a715ed1
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2011-08-24 18:17:32 +0900

    Code cleanup.

--- install.py
+++ install.py
@@ -4,9 +4,8 @@ import re
 
 pattern = re.compile('^.*(~|py|sh|Makefile)$')
 
-for path in glob.glob('*'):
-    if pattern.match(path) == None:
-        dst = os.path.join(os.environ['HOME'], '.' + os.path.basename(path))
-        if os.path.islink(dst):
-            os.remove(dst)
-        os.symlink(os.path.join(os.getcwd(), path), dst)
+for path in [path for path in glob.glob('*') if pattern.match(path) == None]:
+    dst = os.path.join(os.environ['HOME'], '.' + os.path.basename(path))
+    if os.path.islink(dst):
+        os.remove(dst)
+    os.symlink(os.path.join(os.getcwd(), path), dst)
