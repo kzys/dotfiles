bce8e276ef13e9f742ff0dc7c01acade36b46b63
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2011-12-19 17:37:24 +0900

    Extract -newest-dir.

--- emacs.d/init/scala.el
+++ emacs.d/init/scala.el
@@ -1,11 +1,14 @@
-(let* ((dirs (file-expand-wildcards
-             (expand-file-name "~/src/scala-2.*")))
-       (path (car (sort (remove-if-not 'file-directory-p dirs)
-                        (lambda (a b)
-                          (not (string-lessp a b)))))))
-  (cond (path
-         (add-to-list 'load-path
-                      (concat path "/misc/scala-tool-support/emacs/"))
-         (require 'scala-mode)
-         (add-to-list 'auto-mode-alist
-                      '("\\.scala$" . scala-mode)))))
+(defun -newest-dir (pattern subdir)
+  (let* ((dirs (file-expand-wildcards (expand-file-name pattern)))
+         (path (car (sort (remove-if-not 'file-directory-p dirs)
+                          (lambda (a b)
+                            (not (string-lessp a b)))))))
+    (if path (concat path subdir) nil)))
+
+(let ((path
+       (-newest-dir "~/src/scala-2.*" "/misc/scala-tool-support/emacs/")))
+  (cond
+   (path
+    (add-to-list 'load-path path)
+    (require 'scala-mode)
+    (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode)))))
