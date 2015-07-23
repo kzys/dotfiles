8d1d0a2fba6e9363694849e2cfdb1db6b0eef923
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2015-07-22 23:30:40 -0700

    Use flymake-cursor instead of my own function

--- emacs.d/init/00.el
+++ emacs.d/init/00.el
@@ -42,30 +42,7 @@
 
 ;; Flymake
 (require 'flymake)
-
-(defun flymake-display-err-on-minibuffer ()
-  "Displays the error/warning for the current line in the minibuffer"
-  (interactive)
-  (let* ((line-no   
-          (flymake-current-line-no))
-         (line-err-info-list  
-          (nth 0 (flymake-find-err-info flymake-err-info line-no)))
-         (count    
-          (length line-err-info-list))
-         (messages))
-    (while (> count 0)
-      (let* ((info (nth (1- count) line-err-info-list))
-             (file (flymake-ler-file info))
-             (text (flymake-ler-text info))
-             (line (flymake-ler-line info))
-             (full-file (flymake-ler-full-file info)))
-        (setq messages (cons (format "%d: %s" line text) messages)))
-      (setq count (1- count)))
-    (if (> (length messages) 0)
-        (message "%s"
-                 (mapconcat 'identity messages "\n")))))
-
-(run-with-idle-timer 1 t 'flymake-display-err-on-minibuffer)
+(require 'flymake-cursor)
 
 ;; recentf
 (require 'recentf)
