53569b468cfda379990af641876699de4a7aab0d
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-11-11 10:58:44 -0800

    Some apps don't support Emacs-like keybinding

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,44 @@
+local log = hs.logger.new('init.lua','debug')
+
+function createCtrlHotKey(from, to)
+   local f = function ()
+      hs.eventtap.keyStroke({}, to, 50000)
+   end
+   return hs.hotkey.new({'ctrl'}, from, f, nil, f)
+end
+
+emacsLike = {
+   createCtrlHotKey('p', 'UP'),
+   createCtrlHotKey('n', 'DOWN'),
+   createCtrlHotKey('b', 'LEFT'),
+   createCtrlHotKey('f', 'RIGHT'),
+
+   createCtrlHotKey('e', 'END'),
+   createCtrlHotKey('a', 'HOME'),
+}
+
+local APPS_TO_FIX = {
+   ['Microsoft Outlook'] = true,
+   ['Firefox'] = true,
+}
+
+function applicationWatcherCallback(appName, eventType, appObject)
+   if (APPS_TO_FIX[appName]) then
+      if (eventType == hs.application.watcher.activated) then
+         for _, v in pairs(emacsLike) do
+            v:enable()
+         end
+      elseif (eventType == hs.application.watcher.deactivated) then
+         for _, v in pairs(emacsLike) do
+            v:disable()
+         end
+      end
+   end
+end
+
+watcher = hs.application.watcher.new(applicationWatcherCallback)
+watcher:start()
+
 -- https://github.com/Hammerspoon/hammerspoon/issues/1519
 eventtapOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown, hs.eventtap.event.types.otherMouseUp }, function(event)
     local button = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
