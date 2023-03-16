3267cd5dde82cbd033ff8778b4aef8fae4947067
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2023-03-16 08:04:15 -0700

    No company-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -78,7 +78,7 @@
  ;; If there is more than one, they won't work right.
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
  '(package-selected-packages
-   '(company eglot flymake marginalia vertico yaml-mode session scala-mode rust-mode minibuf-isearch magit lua-mode go-mode flymake-cursor)))
+   '(markdown-mode eglot flymake marginalia vertico yaml-mode session scala-mode rust-mode minibuf-isearch magit lua-mode go-mode flymake-cursor)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -88,8 +88,6 @@
 
 (let* ((path (expand-file-name "~/.emacs.d/init-work.el")))
   (if (file-exists-p path) (load path)))
-
-(global-company-mode)
 
 ;; Make eglot work on aws/amazon-ecs-agent where there is agent/go.mod.
 (defun -project-try-gomod (dir)
